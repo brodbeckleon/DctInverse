3b4dd84f24d831043dd00198b70472ab517a41b7
Léon Brodbeck <brodbeckleon@gmail.com>
2023-12-29 18:17:28 +0100

    Changed language from German to English.

--- DctInverse.py
+++ DctInverse.py
@@ -5,8 +5,8 @@ Last Modified: 29.12.2023
 
 import math
 
-# Frequenz und Bild Arrays
-Frequenz = [
+# Frequency und Picture Arrays
+Frequency = [
     [1024, 512, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
@@ -16,29 +16,29 @@ Frequenz = [
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0]
 ]
-Bild = [[0 for _ in range(8)] for _ in range(8)]
+Picture = [[0 for _ in range(8)] for _ in range(8)]
 
 def C(p):
     return 1 / math.sqrt(2) if p == 0 else 1.0
 
-def dct_inverse(Freq, Bild):
+def dct_inverse(Freq, Picture):
     for x in range(8):
         for y in range(8):
             Summe = 0.0
             for u in range(8):
                 for v in range(8):
                     Summe += C(u) * C(v) * Freq[v][u] * math.cos((2.0 * x + 1.0) * u * math.pi / 16.0) * math.cos((2.0 * y + 1.0) * v * math.pi / 16.0)
-            Bild[y][x] = int(round(Summe / 4.0))
+            Picture[y][x] = int(round(Summe / 4.0))
 
-def copyBildToColors(Bild, idctColors, N):
+def copyBildToColors(Picture, idctColors, N):
     for x in range(N):
         for y in range(N):
-            val = Bild[y][x]
+            val = Picture[y][x]
             idctColors[y][x] = min(255, max(0, val))
 
 def get_char_for_value(value):
     chars = "01234567"
-    index = int((255 - value) / 32)  # Skaliert den invertierten Wert auf einen Index von 0 bis 7
+    index = int((255 - value) / 32)  # Scales the inverted value to an index from 0 to 7
     return chars[index]
 
 def print_idct(idctColors, N):
@@ -49,9 +49,9 @@ def print_idct(idctColors, N):
         print()
 
 def main():
-    dct_inverse(Frequenz, Bild)
+    dct_inverse(Frequency, Picture)
     idctColors = [[0 for _ in range(8)] for _ in range(8)]
-    copyBildToColors(Bild, idctColors, 8)
+    copyBildToColors(Picture, idctColors, 8)
     print_idct(idctColors, 8)
 
 if __name__ == "__main__":
